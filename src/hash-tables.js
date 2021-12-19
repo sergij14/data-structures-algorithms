d569454e8844450ffe80e9ee1ce4324e3ceaab12
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-12-19 18:13:17 +0400

    implemented hash table

---
+++ src/hash-tables.js
@@ -0,0 +1,29 @@
+export class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    if (this.data.length) {
+      return this.data[address];
+    }
+    return undefined;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("dagds");
+import { HashTable } from "./hash-tables";
+
+const myHashTable = new HashTable(50);
+myHashTable.set("apples", 1000);
+myHashTable.set("grapes", 1000);
+myHashTable.get("apples");
+
+console.log(myHashTable, myHashTable.get("apples"));
