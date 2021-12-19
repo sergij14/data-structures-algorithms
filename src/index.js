62473037241e2b72c64676b40e120d063c1b5160
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-12-19 18:52:12 +0400

    commented hashtable in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { HashTable } from "./hash-tables";
 
-const myHashTable = new HashTable(50);
-myHashTable.set("apples", 1000);
-myHashTable.set("grapes", 1000);
-myHashTable.get("apples");
+// HASH TABLES
 
-console.log(myHashTable, myHashTable.keys());
+// const myHashTable = new HashTable(50);
+// myHashTable.set("apples", 1000);
+// myHashTable.set("grapes", 1000);
+// myHashTable.get("apples");
+
+// console.log(myHashTable, myHashTable.keys());
