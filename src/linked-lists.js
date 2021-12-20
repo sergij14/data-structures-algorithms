6e9544cf2bb3f8752faa0cbcc72973258d4d32e3
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-12-20 15:11:01 +0400

    added doubly linked list

--- src/linked-lists.js
+++ src/linked-lists.js
@@ -5,6 +5,14 @@ class Node {
   }
 }
 
+class DoublyNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
 export class LinkedList {
   constructor(value) {
     this.head = {
@@ -36,6 +44,63 @@ export class LinkedList {
     const holdingPointer = leader.next;
     leader.next = newNode;
     newNode.next = holdingPointer;
+    this.length++;
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+  }
+}
+
+export class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new DoublyNode(value);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+  prepend(value) {
+    const newNode = new DoublyNode(value);
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = newNode(value);
+    const leader = this.traverseToIndex(index - 1);
+    const follower = leader.next;
+
+    leader.next = newNode;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
     this.length++;
   }
 
