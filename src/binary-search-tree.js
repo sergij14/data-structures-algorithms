db526cd8d92fe31b4b50c4bf151970745686220d
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-12-21 17:55:57 +0400

    init binary search tree

---
+++ src/binary-search-tree.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {}
+  lookup(value) {}
+}
