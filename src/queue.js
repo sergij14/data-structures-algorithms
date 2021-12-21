707211910c82860e4f10aabadb30c4990663995d
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-12-21 13:02:17 +0400

    created queue and its methods

--- src/queue.js
+++ src/queue.js
@@ -11,7 +11,28 @@ class Queue {
     this.last = null;
     this.length = 0;
   }
-  peek() {}
-  enqueue() {}
-  dequeue() {}
+  peek() {
+    return this.first;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+  }
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+    if (this.length === 0) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+  }
 }
