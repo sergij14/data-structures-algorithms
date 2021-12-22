69d7557c6046f6e7acfdd3f448e012f16ac38b0a
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2021-12-22 10:26:36 +0400

    lookup and insert to the tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -27,9 +27,29 @@ export class BinarySearchTree {
           currentNode = currentNode.left;
         } else {
           //right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
         }
       }
     }
   }
-  lookup(value) {}
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+      return false;
+    }
+  }
 }
