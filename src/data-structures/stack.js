2dd42064ea932168efae1c61a9117bb6d18f06ca
sergij14 <sergi.jajanidze@gmail.com>
2022-05-16 18:53:43 +0400

    js stack implementation

--- src/data-structures/stack.js
+++ src/data-structures/stack.js
@@ -1,54 +1,21 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// Stacks
 
-export class Stack {
-  constructor() {
-    this.top = null;
-    this.bottom = null;
-    this.length = 0;
-  }
-  peek() {
-    return this.top;
-  }
-  push(value) {
-    const newNode = new Node(value);
-    if (this.length === 0) {
-      this.top = newNode;
-      this.bottom = newNode;
-    } else {
-      const holdingPointer = this.top;
-      this.top = newNode;
-      this.top.next = holdingPointer;
-    }
-    this.length++;
-  }
-  pop() {
-    if (!this.top) {
-      return null;
-    }
-    if (this.length === 0) {
-      this.bottom = null;
-    }
-    this.top = this.top.next;
-    this.length--;
-  }
-}
+// FILO - first in last out
 
-export class StackArray {
+class Stack {
   constructor() {
-    this.array = [];
+    this.data = [];
   }
+
+  add(el) {
+    this.data.push(el);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+
   peek() {
-    return this.array[this.array.length - 1];
-  }
-  push(value) {
-    this.array.push(value);
-  }
-  pop() {
-    this.array.pop();
+    return this.data[this.data.length - 1];
   }
 }
