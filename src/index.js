487f332019f0e62377ef2401b16a6c77ddf76d3f
sergij14 <sergi.jajanidze@gmail.com>
2022-05-21 20:25:28 +0400

    big 0 hints

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "regenerator-runtime/runtime";
-import { memoize, memoizedAddTo14, spiralMatrix } from "./algorithms";
+import { isPrime, memoize, memoizedAddTo14, spiralMatrix } from "./algorithms";
 import { LinkedList } from "./data-structures/linked-list";
 import { Node, Tree } from "./data-structures/tree";
 
@@ -15,3 +15,6 @@ const memoed = memoize((a,b) => a+b);
 console.log(memoed(14,5));
 console.log(memoed(14,5));
 console.log(memoed(14,3));
+
+console.log(isPrime(5));
+console.log(isPrime(9));
